291ce875d5cbb1058e300d9c33d5a91bac1902f8
cls29 <cls29@st-andrews.ac.uk>
2023-05-20 19:51:49 +0100

    Test tours prep

--- eminipyparser/ETtesttour.py
+++ eminipyparser/ETtesttour.py
@@ -6,17 +6,26 @@ import matplotlib.pyplot as plt
 
 #forms = ["EminiSpec","ASTpy","1DTokens","ASTNX","ASTpyJson","GP2Graph","Neo4j"]
 
-formsGraph = nx.DiGraph()
 
-for val in ET.__dict__.values():
-  if(callable(val)):
-    fromTo = val.__name__.split('To')
-    formsGraph.add_edge(fromTo[0],fromTo[1], func = val.__name__)
+def plotFormatsGraph():  
+    formsGraph = nx.DiGraph()
 
-    #val("find i : int(0..3)")
+    for val in ET.__dict__.values():
+        if(callable(val)):
+            fromTo = val.__name__.split('To')
+            formsGraph.add_edge(fromTo[0],fromTo[1], func = val.__name__)
 
-##print(formsGraph.edges(data=True))
-plt.figure(figsize=(8,8),dpi=80)
+    plt.figure(figsize=(8,8),dpi=80)
 
-nx.draw_circular(formsGraph,with_labels=True, node_size=300)
-plt.show()
+    nx.draw_circular(formsGraph,with_labels=True, node_size=300)
+    plt.show()
+
+def createFormatsGraph():  
+    formsGraph = nx.DiGraph()
+
+    for val in ET.__dict__.values():
+        if(callable(val)):
+            fromTo = val.__name__.split('To')
+            formsGraph.add_edge(fromTo[0],fromTo[1], func = val.__name__)
+
+    return formsGraph
