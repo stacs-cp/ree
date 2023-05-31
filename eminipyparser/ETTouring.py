347e9b5cfdc0e81d538654dee9f804bc31ae3d35
cls29 <cls29@st-andrews.ac.uk>
2023-05-31 16:15:27 +0100

    Heuristic Solver for Chinese Postman problem

--- eminipyparser/ETTouring.py
+++ eminipyparser/ETTouring.py
@@ -5,7 +5,7 @@ from inspect import signature
 import matplotlib.pyplot as plt
 import time
 import collections
-
+import random
 
 class ETGraph:
     '''
@@ -137,48 +137,142 @@ class ETGraph:
                     timeGraph.add_edge(path[-1],neighbour, time = end - start)
                     print(tg)
         return AST, timeGraph
+    
+    def heuristicChinesePostman(self, AST, origin):
+        '''
+        Poor man heuristic to solve the the chinese postman problem.
+        We need a closed cycle that passes over each edge at least once. It is ok if more than once.
+        Likely suboptical.
 
-teststr = """
-letting vertices be domain int(1..3)
-letting colours be domain int(1..3)
-letting G be relation((1,2),(1,3),(2,3))
-letting map be domain relation of (vertices * colours)
-letting T be domain tuple (vertices,colours)
-find C : map
-find t : T
-such that
-  forAll (u,c) in C .
-     forAll (v,d) in C .
-        ((u = v) -> (c = d))
-such that
-  forAll u : vertices .
-     exists c : colours . C(u,c)
-such that
-  forAll (u,v) in G .
-     forAll c,d : colours . (C(u,c) /\ C(v,d) -> (c != d))
-such that
-  t in C
-such that
-  t[1] = t[2]
-"""
+        create stack of edges in the graph
+        start from origin. 
+        while there are elements in the stack
+            pick an edge in the stack
+            compute shortest path to target of edge
+            traverse the path transforming the AST with at each step and remove all edges in the stack encoundered on the way
+        when stack is empty return to origin
+        '''
+        if not nx.is_strongly_connected(self.formsGraph):
+            raise ValueError("Unreachable nodes in the graph")
+        
+        unvisitedEdges = []
+        [unvisitedEdges.append(edge) for edge in self.formsGraph.edges()]
+        currentNode = origin
+        cycle = []
+        cycle.append(origin)
+        while len(unvisitedEdges) > 0:
+            targetEdge = random.choice(unvisitedEdges)
+            ETpath = nx.shortest_path(self.formsGraph, source=currentNode, target=targetEdge[1])
+            
+            for formIndex in range(0,len(ETpath)-1):
+                print("FROM " +ETpath[formIndex]+ " TO " + ETpath[formIndex+1] )
+                print(AST)
+                if type(AST) == ET.ep.Node:
+                    print(ET.ep.printTree(AST,printInfo=True))
+                AST = self.formsGraph[ETpath[formIndex]][ETpath[formIndex+1]]["func"](AST)
+                if (ETpath[formIndex],ETpath[formIndex+1]) in unvisitedEdges:
+                    unvisitedEdges.remove((ETpath[formIndex],ETpath[formIndex+1]))                
+                cycle.append(ETpath[formIndex+1])
+                
+            currentNode = targetEdge[1]
 
-ast = ET.EminiToASTpy(teststr)
-ETG = ETGraph()
+        if currentNode == origin:
+            return cycle
+        else:
+            ETpath = nx.shortest_path(self.formsGraph, source=currentNode, target=origin)
+            for formIndex in range(0,len(ETpath)-1):
+                AST = self.formsGraph[ETpath[formIndex]][ETpath[formIndex+1]]["func"](AST)           
+                cycle.append(ETpath[formIndex+1])
 
-ast2= ETG.allEdgesFrom(ast,"ASTpy")
-#print(ast2[0])
-G= ast2[1]
-#print(ET.ASTpyToEmini(ast2[0]))
-edge_labels = nx.get_edge_attributes(G,'time')
-#print(edge_labels)
+        return cycle
 
-pos = nx.circular_layout(G)
-#nx.draw(G, pos,with_labels=True)
-#nx.draw_networkx_nodes(G, pos, with_labels=True)
-#node_labels = nx.get_node_attributes(G, "label")
-#nx.draw_networkx_labels(G, pos, labels = node_labels)
 
-nx.draw_networkx_edge_labels(G,pos, labels = edge_labels,label_pos=0.3)
+def funcTests():
+    teststr = """
+    letting vertices be domain int(1..3)
+    letting colours be domain int(1..3)
+    letting G be relation((1,2),(1,3),(2,3))
+    letting map be domain relation of (vertices * colours)
+    letting T be domain tuple (vertices,colours)
+    find C : map
+    find t : T
+    such that
+    forAll (u,c) in C .
+        forAll (v,d) in C .
+            ((u = v) -> (c = d))
+    such that
+    forAll u : vertices .
+        exists c : colours . C(u,c)
+    such that
+    forAll (u,v) in G .
+        forAll c,d : colours . (C(u,c) /\ C(v,d) -> (c != d))
+    such that
+    t in C
+    such that
+    t[1] = t[2]
+    """
 
-nx.draw_circular(G,with_labels=True, node_size=300, connectionstyle="arc3,rad=-0.1" )
-plt.show()
+    ast = ET.EminiToASTpy(teststr)
+    ETG = ETGraph()
+
+    ast2= ETG.allEdgesFrom(ast,"ASTpy")
+    #print(ast2[0])
+    G= ast2[1]
+    #print(ET.ASTpyToEmini(ast2[0]))
+    edge_labels = nx.get_edge_attributes(G,'time')
+    #print(edge_labels)
+
+    pos = nx.circular_layout(G)
+    #nx.draw(G, pos,with_labels=True)
+    #nx.draw_networkx_nodes(G, pos, with_labels=True)
+    #node_labels = nx.get_node_attributes(G, "label")
+    #nx.draw_networkx_labels(G, pos, labels = node_labels)
+
+    nx.draw_networkx_edge_labels(G,pos, labels = edge_labels,label_pos=0.3)
+
+    nx.draw_circular(G,with_labels=True, node_size=300, connectionstyle="arc3,rad=-0.1" )
+    plt.show()
+
+def postMantest():
+    teststr = """
+    letting vertices be domain int(1..3)
+    letting colours be domain int(1..3)
+    letting G be relation((1,2),(1,3),(2,3))
+    letting map be domain relation of (vertices * colours)
+    letting T be domain tuple (vertices,colours)
+    find C : map
+    find t : T
+    such that
+    forAll (u,c) in C .
+        forAll (v,d) in C .
+            ((u = v) -> (c = d))
+    such that
+    forAll u : vertices .
+        exists c : colours . C(u,c)
+    such that
+    forAll (u,v) in G .
+        forAll c,d : colours . (C(u,c) /\ C(v,d) -> (c != d))
+    such that
+    t in C
+    such that
+    t[1] = t[2]
+    """
+
+    ast = ET.EminiToASTpy(teststr)
+    ETG = ETGraph()
+    cycle = ETG.heuristicChinesePostman(ast,"ASTpy")
+    pos = nx.circular_layout(ETG.formsGraph)
+    #nx.draw(G, pos,with_labels=True)
+    #nx.draw_networkx_nodes(G, pos, with_labels=True)
+    #node_labels = nx.get_node_attributes(G, "label")
+    #nx.draw_networkx_labels(G, pos, labels = node_labels)
+    path_edges = list(zip(cycle,cycle[1:]))
+    print(cycle)
+    nx.draw(ETG.formsGraph,pos, with_labels=True)
+
+    path_edges = list(zip(cycle,cycle[1:]))
+    #nx.draw_networkx_nodes(ETG.formsGraph,pos,nodelist=cycle,node_color='r')
+    nx.draw_networkx_edges(ETG.formsGraph,pos,edgelist=path_edges,edge_color='g',alpha=0.3,width=2,connectionstyle="arc3,rad=-0.1")
+    plt.show()
+
+#postMantest()
