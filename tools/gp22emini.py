9aa22db0c7534aea94cba87546dacd9e64912269
Andras Salamon <Andras.Salamon@st-andrews.ac.uk>
2023-06-05 17:54:27 +0100

    gp2 to emini

---
+++ tools/gp22emini.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import sys
+import fileinput
+sys.path.append('tools')
+import EFormatGraph
+
+teststr = ''
+ETG = EFormatGraph.ETGraph()
+with fileinput.input() as f:
+    for l in f:
+        teststr += l
+GP2 = ETG.FormToForm(teststr,"GP2String","Emini")
+print(GP2)
