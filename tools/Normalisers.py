e8c522bc99a0e8fc65683c40bfb6d69f96349df5
cls00 <cls29@st-andrews.ac.uk>
2023-10-11 17:22:25 +0100

    Preparing normalisers

--- tools/Normalisers.py
+++ tools/Normalisers.py
@@ -5,18 +5,19 @@ statementsOrder = {"NameLettingStatement": 1,
                    "DomainNameLettingStatement" :2,
                    "FindStatement" : 3,
                    "SuchThatStatement" : 4}
-# Order Chars <symbols< numbers
+# Order literals < ops < digits
 binaryOpNormalisable = ["*","+","=", "!=", "/\\","\\/"]
 
 def normaliseASTpy(node):
-    if len(node.Children) >= 2:
-        if node.info in normalisable:
-            node.children = reOrder(node)
+    if node.info in normalisable:
+        node.children = reOrder(node)
+    for child in node.children:
+        normaliseASTpy(child)
 
 
 def reOrder(node):
     if node.info == "RelationConstant":
-        print("what to do with the tuple children as they are indistinguishable")
+        print("what to do with the tuples ?")
     elif node.info == "BinaryExpression":
         if node.label in binaryOpNormalisable:
             node.children = sorted(node.children,  key=lambda x: x.label)
@@ -26,3 +27,7 @@ def reOrder(node):
 
 def compareStatements(x,y):
     return statementsOrder[x] - statementsOrder[y]
+
+## place holder
+def compareExpression(exp1, exp2):
+    return 0
