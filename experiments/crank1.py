fe4302f9eb219072d2d7abe8bb5f4e1bb1c1ab74
cls00 <cls29@st-andrews.ac.uk>
2024-01-09 14:19:58 +0000

    crank tests

--- experiments/crank1.py
+++ experiments/crank1.py
@@ -22,15 +22,20 @@ import EFormatGraph as EFG
 import eminipyparser as ep
 import os
 import GP2Interface
-
-
+import pandas as pd
+import time
 
 def SolveAndTransform() -> int:
 
 ## Create Reformulation Graph (node: spec, edge:transform)
 ## Actions: generate spec, generate instance, transform spec, solve instance
+    # event: type, time, space,details
+    # events: generate instance (abstract spec to constrete spec), translate (Format to format), transform ( spec to spec), solve (concrete spec to solution)
+    # 
     
     # test spec update to generator call
+    start = time.time_ns() 
+
     spec = '''
     find i : int(0..100)
     such that
@@ -41,6 +46,7 @@ def SolveAndTransform() -> int:
     such that
         a = !(b /\ c)'''
 
+    parentID = hash(spec)
     # save spec to file. store file size + time
     specFilename = "./tests/testExpression.essence"
     with open(specFilename, 'w') as file:
@@ -50,13 +56,18 @@ def SolveAndTransform() -> int:
     params = ""
     conjureCall = ['conjure','solve', specFilename]
     subprocess.run(conjureCall, check=True)
+    solveTime =time.time_ns() - start
+
 
     # TODO upgrade to log solution. time, size, number of nodes traversed
     with open("./conjure-output/model000001-solution000001.solution") as solution:
-        print(solution.read())
+        s = solution.read()
+        parentSolutionID = hash(s)
+        print(s)
 
     formatsGraph = EFG.ETGraph()
 
+    start2 = time.time_ns()
     # Translate to GP2 (could be done in parallel)
     gp2spec = formatsGraph.FormToForm(spec,"Emini","GP2String")
     gp2hostfile = "./gp2/testExpression.host"
@@ -69,26 +80,29 @@ def SolveAndTransform() -> int:
     GP2Interface.runPrecompiledProg(progName,hostGraph)
 
     gp2specNEW = ""
-    
+    transformTime = time.time_ns() - start2
     # If trasform is applicable solve new spec
     if os.path.isfile("gp2.output"):
         with open("gp2.output") as newGP2spec:
             gp2specNEW = newGP2spec.read()
         print(gp2specNEW)
         spec2 = formatsGraph.FormToForm(gp2specNEW,"GP2String","Emini")
+        instanceID = hash(spec2)
         print(spec2)
         spec2Filename = "./tests/testExpression2.essence"
         with open(spec2Filename, 'w') as file:
             file.write(spec2)
-
+        start3 = time.time_ns()
         # call conjure and solve spec
         conjureCall2 = ['conjure','solve', spec2Filename]
         subprocess.run(conjureCall2, check=True)
 
         # TODO upgrade to log solution
         with open("./conjure-output/model000001-solution000001.solution") as solution:
-            print(solution.read())
-        
+            s = solution.read()
+            solutionID = hash(s)
+            print(s)
+        instanceSolveTime = time.time_ns() - start3
         # Clear files
         os.remove("gp2.output")
         if os.path.isfile("gp2.log"):
@@ -96,8 +110,25 @@ def SolveAndTransform() -> int:
     else:
         # TODO add log
         print("Transform not applied")
+    return {'instanceID': instanceID, 
+                'parentID': parentID, 
+                'trasform': progName, 
+                'transformTime':transformTime, 
+                'parentSolveTime' : solveTime,
+                'instanceSolveTime':  instanceSolveTime,
+                'parentSolutionID':parentSolutionID, 
+                'solutionID':solutionID }
+    
 
     
 if __name__ == '__main__':
-    sys.exit(SolveAndTransform())
+    # define columns
+    columns = ['instanceID', 'parentID', 'trasform', 'transformTime', 'parentSolveTime', 'instanceSolveTime','parentSolutionID', 'solutionID']  
+    data_rows = []
+    for i in range(20):
+        data_rows.append(SolveAndTransform())
+    print(data_rows)
+    dataLogger = pd.DataFrame(data_rows,columns = columns)
+    dataLogger.to_csv("experiments/crankSanityCheck.csv")
+    sys.exit()
     
