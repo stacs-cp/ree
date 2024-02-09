a461d074a98cfd6892ec5edfb0637300de1692ca
cls00 <cls29@st-andrews.ac.uk>
2024-02-09 21:02:20 +0000

    added filters to profiler

--- greee/dev-debug/call_graph_logger.py
+++ greee/dev-debug/call_graph_logger.py
@@ -1,5 +1,8 @@
 from pycallgraph2 import PyCallGraph
 from pycallgraph2.output import GraphvizOutput
+from pycallgraph2 import GlobbingFilter
+from pycallgraph2 import Config
+
 
 import sys
 sys.path.append('.')
@@ -10,12 +13,30 @@ import time
 import matplotlib.pyplot as plt
 from greee import essence_transforms
 import networkx as nx
-graphviz = GraphvizOutput()
 
-with PyCallGraph(graphviz):
+
+graphviz = GraphvizOutput()
+config = Config()
+
+
+trace_filter = GlobbingFilter(exclude=[
+    'pycallgraph.*',
+    're.*',
+    'contextlib.*',
+    'itemsview.*',
+    'subprocess.Popen._*',
+    '__*'
+])
+config.trace_filter = trace_filter
+
+
+
+with PyCallGraph(config=config,output=graphviz):
+
+
     graphviz.output_file = 'output.svg'
     graphviz.output_type = 'svg'
-    etransform_graph = essence_transforms.EssenceTransforms()
+    et = essence_transforms.EssenceTransforms()
 
     spec = r'''find i : int(0..100)
     such that
@@ -32,13 +53,14 @@ with PyCallGraph(graphviz):
     with open("StartSpec.essence", 'w') as file:
         file.write(spec)
 
-    spec_ID = etransform_graph.add_e_node(spec,"StartSpec.essence")
+    spec_ID = et.add_e_node(spec,"StartSpec.essence")
     #solution = etransform_graph.solve(spec_ID)
     #solveTime =time.time_ns() - start
     #parentSolutionID = hash(solution)
 
-    print("MAB")
-    for _ in range(0,5):
-        etransform_graph.expand_from_node(spec_ID)
-    print(etransform_graph.graph.nodes(data=True))
+
+    for _ in range(0,100):
+        selected_node = et.select_current_node()
+        et.expand_from_node(selected_node)
+    print(et.graph.nodes(data=True))
 
