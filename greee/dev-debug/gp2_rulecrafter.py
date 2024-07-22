34383a44eef3cd83e594681db5a84122779aaaec
cls00 <cls29@st-andrews.ac.uk>
2024-07-22 15:41:53 +0100

    added rule precursor for GP2StringB format

--- greee/dev-debug/gp2_rulecrafter.py
+++ greee/dev-debug/gp2_rulecrafter.py
@@ -3,6 +3,9 @@ sys.path.append('.')
 
 from greee import EFormatGraph
 from greee import essence2rules_tools
+import networkx as nx
+import matplotlib.pyplot as plt
+
 gcmulti = r"""
 
 
@@ -12,12 +15,16 @@ such that
 """
 EFG = EFormatGraph.EFGraph()
 gp2g = EFG.FormToForm(gcmulti, "Emini","GP2Graph")
-gp2_partial_rule = essence2rules_tools.GP2GraphToGP2StringDT_rule_precursor(gp2g)
+gp2_partial_rule = essence2rules_tools.GP2GraphToGP2StringB_rule_precursor(gp2g)
 print("______Partial Rule____\n" ,gp2_partial_rule)
 
 preserved_nodes= [0,1,2,3,4,5]
-gp2_partial_rule2 = essence2rules_tools.GP2GraphToGP2StringDT_rule_precursor(gp2g,preserved_nodes)
+gp2_partial_rule2 = essence2rules_tools.GP2GraphToGP2StringB_rule_precursor(gp2g,preserved_nodes)
 print("______Partial Rule Chopped____\n", gp2_partial_rule2)
-eministr = EFG.FormToForm(gp2_partial_rule2, "GP2StringDT","Emini")
+nxLeftover = EFG.FormToForm(gp2_partial_rule2,"GP2StringB", "NX")
+print("____", len(nxLeftover.nodes))
+nx.draw(nxLeftover, with_labels = True)
+plt.show()
+eministr = EFG.FormToForm(gp2_partial_rule2, "GP2StringB","Emini")
 print("______New Essence____\n",eministr)
 
