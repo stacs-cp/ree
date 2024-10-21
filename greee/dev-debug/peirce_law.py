21ac7000ecff6ac994d21224dce1979b186177a4
cls00 <cls29@st-andrews.ac.uk>
2024-10-21 15:50:11 +0100

    added recursive clearance of flags in peirce law

--- greee/dev-debug/peirce_law.py
+++ greee/dev-debug/peirce_law.py
@@ -7,14 +7,14 @@ from greee import essence_transforms
 from greee import eminipyparser
 progName = "peirce.gp2"
 
-gp2Interface.compileGP2Program(progName)
+#gp2Interface.compileGP2Program(progName)
 
 peirce_test = r"""
 letting X be false
 letting Q be true
 find P : bool
 such that
- (P -> Q) -> X """
+ (X -> Q) -> P """
 
 EFG = EFormatGraph.EFGraph()
 ast1 = EFG.FormToForm(peirce_test, "Emini","ASTpy")
@@ -35,6 +35,10 @@ eminipyparser.printTree(new_ast)
 
 def simplify(ast):
     def findRed(node, found=[]):
+        '''
+        Returns a list of roots of subtrees that have been flagged
+        They will be later check for equality
+        '''
         content = node.label.split("# ")
         if len(content)>1:
             if content[1] == "red":
@@ -44,18 +48,31 @@ def simplify(ast):
         return found
 
     def findGrey(node, found=[]):
+        '''
+        Returns a list of parents of the roots of subtrees that have been flagged
+        If their children are equal a simplification will be triggered
+        '''
         content = node.label.split("# ")
         if len(content)>1:
             if content[1] == "grey":
                 found.append(node)
-                print(node)
+                print(node.label)
         for n in node.children:
             findGrey(n,found)
         return found
+    
+    def clearFlags(node):
+        '''
+        Clear all left over flags
+        '''
+        node.label = node.label.split("# ")[0] #this removes a flag if any
+        for n in node.children:
+            clearFlags(n)
 
 
     parentFound = findGrey(new_ast)
     candidate_subtrees = findRed(ast)
+    eminipyparser.printTree(ast)
     if eminipyparser.treeEquality(candidate_subtrees[0],candidate_subtrees[1]):
         for i,child in enumerate(parentFound[0].children):
             content = child.label.split("# ")
@@ -67,7 +84,7 @@ def simplify(ast):
                     parentFound[0].children[i].label = parentFound[0].children[i].label.split("# ")[0]
                     # remove grey flag
                     parentFound[0].label = parentFound[0].label.split("# ")[0]
-            print(parentFound[0])
+            print("parent ", parentFound[0].label)
     else:
         for i,child in enumerate(parentFound[0].children):
             content = child.label.split("# ")
@@ -75,13 +92,16 @@ def simplify(ast):
                 if content[1] == "grey":
                     # Add blue flag
                     parentFound[0].children[i].label = parentFound[0].children[i].label.split("# ")[0]
-                    parentFound[0].children[i].label += "#blue"
+                    parentFound[0].children[i].label += "# blue"
                     # remove grey flag
                     parentFound[0].label = parentFound[0].label.split("# ")[0]
                 
-            print(parentFound[0])
+            print("unequal parent ", parentFound[0].label)
     
 
     eminipyparser.printTree(ast)
+    clearFlags(ast)
+    eminipyparser.printTree(ast)
+
 
 simplify(new_ast)
