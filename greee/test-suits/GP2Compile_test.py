ec6942715fb82a78a11a65ffe376b64fa1b8503b
cls00 <cls29@st-andrews.ac.uk>
2025-01-16 17:22:23 +0000

    updated compile test

--- greee/test-suits/GP2Compile_test.py
+++ greee/test-suits/GP2Compile_test.py
@@ -3,6 +3,6 @@ import sys
 sys.path.append('.')
 from greee import gp2Interface
 
-progName = "gcmultiRelToFunc7.gp2"
+progName = "greee/gp2/gcmultiRelToFunc7.gp2"
 
 gp2Interface.compileGP2Program(progName)
